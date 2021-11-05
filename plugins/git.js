0b342761fe7750ebefbe3a17d1fd9d90b5b78ce4
Arjunser5 <92748603+Arjunser5@users.noreply.github.com>
2021-11-05 18:15:06 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,17 +11,17 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/VJuPuTP.jpg";
+  r_text[0] = "Arjunser.png";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*😈𝕊ℍ𝔸ℍ𝕀𝔻-𝕊ℍ𝔸ℤℤ😈*
-*𝕆𝕨𝕟𝕖𝕣 𝕟𝕦𝕞𝕓𝕖𝕣 wa.me/918078391406?text=Hi*
-*𝕊𝕙𝕒𝕙𝕚𝕕-𝕤𝕙𝕒𝕫𝕫 𝔻𝕖𝕒𝕕𝕡𝕠𝕠𝕝 𝕘𝕚𝕥𝕙𝕦𝕓𝕝𝕚𝕟𝕜   https://github.com/shazz-shahid/Deadpool*
-*𝔸𝕞𝕖𝕖𝕣-𝕜𝕒𝕝𝕝𝕦𝕞𝕥𝕙𝕠𝕕𝕚 𝕡𝕚𝕜𝕒𝕔𝕙𝕦 𝕘𝕚𝕥𝕙𝕦𝕓𝕝𝕚𝕟𝕜 https://github.com/ameer-kallumthodi/Pikachu*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*😈𝐴𝑅𝐽𝑈𝑁𝑆𝐸𝑅😈*
+*𝕆𝕨𝕟𝕖𝕣 𝕟𝕦𝕞𝕓𝕖𝕣 http://api.whatsapp.com/send?phone=+916238440754&text=🌹--𝐻𝐸𝐿𝐿𝑂-𝐴𝑅𝐽𝑈𝑁𝑆𝐸𝑅*
+*𝘉𝘖𝘛 𝘒𝘏𝘐𝘓𝘈𝘋𝘐𝘐 𝑨𝑹𝑱𝑼𝑵𝑺𝑬𝑹 𝕘𝕚𝕥𝕙𝕦𝕓𝕝𝕚𝕟𝕜   https://github.com/Arjunser5/Arjunser1*
+*𝑂𝑊𝑁𝐸𝑅 - 𝘈𝘙𝘑𝘜𝘕𝘚𝘌𝘙🍒💕*
 `}) 
 
 }));
