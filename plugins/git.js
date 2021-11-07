69d0fc82db401c7c6fcb32fcd3507c7cef30a078
Arjunser5 <92748603+Arjunser5@users.noreply.github.com>
2021-11-07 16:08:40 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "http://imgur.com/a/1n7sG67.png";
+  r_text[0] = "http://imgur.com/a/1n7sG67.jpg";
     
     
     var i = Math.floor(1*Math.random())
